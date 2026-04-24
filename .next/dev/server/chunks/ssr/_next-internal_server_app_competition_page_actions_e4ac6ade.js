91c84f65c6bb2b883094bb61d6bcf60e17be055a
Shiven Khurana <shivenbk@gmail.com>
2026-04-23 23:03:09 -0400

    update

--- .next/dev/server/chunks/ssr/_next-internal_server_app_competition_page_actions_e4ac6ade.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/competition/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_competition_page_actions_e4ac6ade.js.map
